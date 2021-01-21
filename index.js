5e1431f2556d2f25be99fe2d549adf3b089a797b
miqdad71 <miqdad.ihbs71777@gmail.com>
2021-01-21 09:37:28 +0700

    delete duplicate import

--- index.js
+++ index.js
@@ -11,17 +11,12 @@ const customerRouter = require('./src/routes/customer')
 const adminRouter = require('./src/routes/admin')
 const productRouter = require('./src/routes/product')
 
-// import morgan dan CORS
-const morgan = require('morgan')
-const cors = require('cors')
-
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(morgan('dev'))
 app.use(cors())
 // app.use('/image', express.static('./uploads'))
 app.use('/images', express.static('./uploads'))
-
 
 // Config CORS
 app.use((req, res, next) => {
@@ -32,7 +27,6 @@ app.use((req, res, next) => {
   )
   next()
 })
-
 
 app.use('/product', productRouter)
 app.use('/account', accountRouter)
