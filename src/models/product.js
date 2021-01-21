65f403cb88c266b6ce1c57a3bbdd1b1fcca20a0e
Ikrima Amanda <ikrima.amanda.07@gmail.com>
2021-01-21 08:47:25 +0800

    Add search

--- src/models/product.js
+++ src/models/product.js
@@ -1,9 +1,28 @@
 const db = require('../helpers/db')
 
 module.exports = {
-  getAllProductModel: () => {
+  getAllProductModel: (paginate) => {
     return new Promise((resolve, reject) => {
-      const query = 'SELECT * FROM product'
+      const query = `SELECT * FROM product 
+      GROUP BY pr_name
+      LIMIT ${paginate.limit}
+      OFFSET ${paginate.offset}`
+
+      db.query(query, (err, result, fields) => {
+        if (!err) {
+          resolve(result)
+        } else {
+          reject(new Error(err))
+        }
+      })
+    })
+  },
+  getSearchProductModel: (paginate) => {
+    return new Promise((resolve, reject) => {
+      const query = `SELECT * FROM product WHERE pr_name LIKE '%${paginate.search}%'
+      GROUP BY pr_name
+      LIMIT ${paginate.limit}
+      OFFSET ${paginate.offset}`
 
       db.query(query, (err, result, fields) => {
         if (!err) {
