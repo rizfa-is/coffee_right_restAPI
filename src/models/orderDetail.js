e65aee3c20b4f383d11cc294760b1cb617e85b86
miqdad71 <miqdad.ihbs71777@gmail.com>
2021-02-04 22:44:22 +0700

    update model order detail

--- src/models/orderDetail.js
+++ src/models/orderDetail.js
@@ -4,6 +4,12 @@ const {
   historyOrderByOdId
 } = require('../models/order')
 
+const dbConnect = require('../config/db')
+
+const {
+  historyOrderByOdId
+} = require('../models/order')
+
 module.exports = {
 
   getAllOrderDetail: () => {
@@ -25,12 +31,46 @@ module.exports = {
   getAllOrderDetailByCsIdModel: (csId) => {
     return new Promise((resolve, reject) => {
       const query = `
-      SELECT * FROM order_detail WHERE cs_id = '${csId}'
+      SELECT od.od_id, od.cs_id, dv.dv_dt, dv.dv_address, od.od_total_price_before_tax, od.od_totalPrice, ac.ac_name, ac.ac_email, ac.ac_phone,
+      od.od_transaction_id, od.od_status, od.od_payment_method, od.od_tax, od.od_created_at, od.od_updated_at FROM order_detail od 
+      JOIN order_tb o ON (od.od_id = o.od_id)
+      JOIN product pr ON (o.pr_id = pr.pr_id)
+      JOIN delivery dv ON (od.dv_id = dv.dv_id)
+      JOIN customer cs ON (od.cs_id = cs.cs_id)
+      JOIN account ac ON (ac.ac_id = cs.ac_id)
+      WHERE cs_id = '${csId}'
       `
 
       dbConnect.query(query, (error, results, _fields) => {
         if (!error) {
-          resolve(results)
+          const data = []
+
+          for (let i = 0; i < results.length; i++) {
+            const item = results[i]
+
+            const order = await historyOrderByOdId(item.od_id)
+
+            data[i] = {
+              od_id: item.od_id,
+              cs_id: item.cs_id,
+              dv_dt: item.dv_dt,
+              cs_name: item.ac_name,
+              cs_email: item.ac_email,
+              cs_phone: item.ac_phone,
+              dv_address: item.dv_address,
+              od_total_price_before_tax: item.od_total_price_before_tax,
+              od_transaction_id: item.od_transaction_id,
+              od_total_price: item.od_totalPrice,
+              od_status: item.od_status,
+              od_payment_method: item.od_payment_method,
+              od_tax: item.od_tax,
+              od_created_at: item.od_created_at,
+              od_updated_at: item.od_updated_at,
+              product_order: order
+            }
+            i++
+          }
+          resolve(data)
         } else {
           reject(error)
         }
@@ -46,7 +86,8 @@ module.exports = {
       JOIN order_tb o ON (od.od_id = o.od_id)
       JOIN product pr ON (o.pr_id = pr.pr_id)
       JOIN delivery dv ON (od.dv_id = dv.dv_id)
-      JOIN customer cs ON (od.cs_id = cs.cs_id) JOIN account ac ON (ac.ac_id = cs.ac_id)
+      JOIN customer cs ON (od.cs_id = cs.cs_id)
+      JOIN account ac ON (ac.ac_id = cs.ac_id)
       WHERE od.od_id = ${odId}
       `
 
@@ -62,10 +103,10 @@ module.exports = {
             data[i] = {
               od_id: item.od_id,
               cs_id: item.cs_id,
+              dv_dt: item.dv_dt,
               cs_name: item.ac_name,
               cs_email: item.ac_email,
               cs_phone: item.ac_phone,
-              dv_dt: item.dv_dt,
               dv_address: item.dv_address,
               od_total_price_before_tax: item.od_total_price_before_tax,
               od_transaction_id: item.od_transaction_id,
@@ -164,3 +205,4 @@ module.exports = {
   }
 
 }
+
