6d434c3e2d3c63d6393a007312681793d162e25e
rizfa-is <67631569+rizfa-is@users.noreply.github.com>
2021-02-06 08:25:47 +0700

    update response get all order detail

--- src/models/orderDetail.js
+++ src/models/orderDetail.js
@@ -9,12 +9,44 @@ module.exports = {
   getAllOrderDetail: () => {
     return new Promise((resolve, reject) => {
       const query = `
-      SELECT * FROM order_detail
+      SELECT od.od_id, od.cs_id, dv.dv_dt, dv.dv_address, od.od_total_price_before_tax, od.od_totalPrice, ac.ac_name, ac.ac_email, ac.ac_phone,
+      od.od_transaction_id, od.od_status, od.od_payment_method, od.od_tax, od.od_created_at, od.od_updated_at FROM order_detail od 
+      JOIN order_tb o ON (od.od_id = o.od_id)
+      JOIN product pr ON (o.pr_id = pr.pr_id)
+      JOIN delivery dv ON (od.dv_id = dv.dv_id)
+      JOIN customer cs ON (od.cs_id = cs.cs_id)
+      JOIN account ac ON (ac.ac_id = cs.ac_id)
       `
 
-      dbConnect.query(query, (error, results, _fields) => {
+      dbConnect.query(query, async(error, results, _fields) => {
         if (!error) {
-          resolve(results)
+          const data = []
+
+          for (let i = 0; i < results.length; i++) {
+            const item = results[i]
+
+            const order = await historyOrderByOdId(item.od_id)
+
+            data[i] = {
+              od_id: item.od_id,
+              cs_id: item.cs_id,
+              dv_dt: item.dv_dt,
+              cs_name: item.ac_name,
+              cs_email: item.ac_email,
+              cs_phone: item.ac_phone,
+              dv_address: item.dv_address,
+              od_total_price_before_tax: item.od_total_price_before_tax,
+              od_transaction_id: item.od_transaction_id,
+              od_total_price: item.od_totalPrice,
+              od_status: item.od_status,
+              od_payment_method: item.od_payment_method,
+              od_tax: item.od_tax,
+              od_created_at: item.od_created_at,
+              od_updated_at: item.od_updated_at,
+              product_order: order
+            }
+          }
+          resolve(data)
         } else {
           reject(error)
         }
@@ -65,7 +97,6 @@ module.exports = {
           }
           resolve(data)
         } else {
-          console.log(error);
           reject(error)
         }
       })
